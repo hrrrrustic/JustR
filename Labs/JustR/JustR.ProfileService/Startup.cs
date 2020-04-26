de839bf53b5e70030533409190ec59cbff0d2d11
hrrrustic <hrrrrustic@gmail.com>
2020-04-26 18:39:12 +0000

    csproj
    git-tfs-id: [https://dev.azure.com/veeamacademy/]$/Vladislav.Kazansky;C126

--- Labs/JustR/JustR.ProfileService/Startup.cs
+++ Labs/JustR/JustR.ProfileService/Startup.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JustR.ProfileService.Repository;
+using JustR.ProfileService.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SqlKata.Compilers;
 
 namespace JustR.ProfileService
 {
@@ -16,6 +19,9 @@ namespace JustR.ProfileService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<Compiler, SqlServerCompiler>();
+            services.AddScoped<IProfileRepository, ProfileRepository>();
+            services.AddScoped<IProfileService, Service.ProfileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,14 +33,6 @@ namespace JustR.ProfileService
             }
 
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
         }
     }
 }
